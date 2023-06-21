176c9cf0c26479b0c380924262552f8c4e16b3b5
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-06-21 16:12:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded", function() {
   // Check if the body tag is empty
-  var bodyIsEmpty = document.body.innerHTML.trim() === "";
-
-  if (bodyIsEmpty) {
-    // Write the string on the web page
-    var message = "DOM load success";
-    var paragraph = document.createElement("p");
-    var textNode = document.createTextNode(message);
-    paragraph.appendChild(textNode);
-    document.body.appendChild(paragraph);
-  }
+  document.body.innerText="DOM load success";
 });
